ed6483bebc68e5f97ca918136319164942ae4380
FLorine-R28 <rodriguesflorine88pgmail.com>
2023-01-07 11:55:49 +0100

    version 1

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-const init = () => {
-  // Your code here ...
-}
+let counter = 0;
+const lessBtn = document.getElementById('less-button');
+const resetBtn = document.getElementById('reset-button');
+const addBtn = document.getElementById('add-button');
+const totalScore = document.getElementById('total');
 
-window.onload = init;
+addBtn.addEventListener('click', function() {
+  counter++;  
+  totalScore.innerText = counter;
+})
+
+lessBtn.addEventListener('click', function() {
+  counter--;
+  if (counter < 0){
+    counter = 0;
+  }
+  totalScore.innerText = counter;
+})
+
+resetBtn.addEventListener('click', function(){
+  counter = 0; 
+  totalScore.innerText = counter;
+})
+
